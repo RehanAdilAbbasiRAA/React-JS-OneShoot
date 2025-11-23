be7bcbbf71c926de016bc00766dab71f77b78bdd
Rehan Adil AbbasiRAA <138215723+RehanAdilAbbasiRAA@users.noreply.github.com>
2025-11-24 00:15:09 +0500

    got

--- w4day28/src/main.jsx
+++ w4day28/src/main.jsx
@@ -10,7 +10,7 @@ import { store } from "./app/store";
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode>
     <BrowserRouter>
     <ThemeProvider>
       <Provider store={store}>
@@ -20,5 +20,5 @@ createRoot(document.getElementById('root')).render(
     </ThemeProvider>
       
     </BrowserRouter>
-  </StrictMode>,
+  // </StrictMode>,
 )
